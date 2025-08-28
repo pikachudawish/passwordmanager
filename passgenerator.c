ddb6846749153bd14bede26f83c1b8aa56908247
pikachudawish <ruben.costa.820@gmail.com>
2025-08-28 01:32:35 +0100

    starting

---
+++ passgenerator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct infos {
+    char* app;
+    char* pass;
+}password;
+
+
+int  main() {
+    int opt = 0;
+    
+
+    switch(opt) {
+        case 1:
+
+        break;
+
+
+
+        case 2:
+
+        break;
+    }
+
+
+    return 0;
+}
